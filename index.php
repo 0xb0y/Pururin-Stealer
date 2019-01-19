0dea70a35902930083253e07beba63b536a9647c
{ <bibsindra@gmail.com>
2019-01-20 01:10:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<title>Pururin.io Stealer mirror</title>
+<link rel="shortcut icon" href="https://4.bp.blogspot.com/-DweioerHPNg/XEAf4rEqqqI/AAAAAAAABLc/d_BTxB3uqro1gxaU-iSgK0Apu3YA7pMfgCLcBGAs/s1600/slemanb0ys-ico.jpg" type="image/x-icon">
+<meta name='author' content='Sinkaroid Stupidc0de'>
+<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+
+
+<style>
+
+.subbtn {background:#b70505;color:white;border: 1px solid #000; padding:6px 6px 6px 6px;}
+
+
+.bordergaya{
+			background:black;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid #2d2b2b;
+	
+		}
+		.bordergaya:hover{
+			background:#2d2b2b;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid crimson;
+		}
+	
+
+.image {
+    width: 400px;
+    height: 350px;
+    -webkit-animation:spin 8s linear infinite;
+    -moz-animation:spin 8s linear infinite;
+    animation:spin 5s linear infinite;
+}
+@-moz-keyframes spin { 100% { -moz-transform: rotate(-360deg); } }
+@-webkit-keyframes spin { 100% { -webkit-transform: rotate(-360deg); } }
+@keyframes spin { 100% { -webkit-transform: rotate(-360deg); transform:rotate(-360deg); } }
+</style>
+
+<center><font face="Ubuntu" size="5" color="green"><b>[!] pUrurin.io Mirror [!]<b/></font></center>
+<br>
+<body bgcolor ="black">
+<center><font face="Ubuntu" size="4" color="green"><b><i>a sec till complete.. then, go put in pUrurin grabber</i><b/></font></center>
+<center><a href="grab.php" rel="nofollow" target="_blank"><font face="Ubuntu" size="4" color="white"><b>[ grab here ]<b/></font></a><center>
+<p>
+<center>
+<?php
+for ($i= 1; $i <= 24; $i++) ///IMPORTANT ,check the folder, see how many pictures there are,  change the variable [24] to max numb in pururin.io gallery
+{
+echo" <img width='220' height='350' src=";
+/**geturl**/ echo "https://api.pururin.io/images";
+echo "/38027/"; ///put code here 
+echo $i; echo".jpg"; ///check nh folder first, if png format just change it
+echo ">";
+echo "";
+}
+
+
+
+?>
+<p>
+<center><font face="Ubuntu" size="5" color="green"><b>[!] pUrurin.io Stealer [!]<b/></font></center>
+<br>
+<center><font face="Ubuntu" size="3" color="green"><b><i>(c) 2019 coded by sinkaroid ~penjagabisu@outlook.com</i><b/></font></center>
+<center><img class='image' src='https://3.bp.blogspot.com/-6XZOk6uPV4U/XEBO8xwFcwI/AAAAAAAABL4/BDAHAxIoOCAZCO0FmEnNtAyGXoTx9dz6wCLcBGAs/s1600/weeblogo.png' width='600' height='520'/>
